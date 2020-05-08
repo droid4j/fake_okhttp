96a46391836af8c2bf0c5e9ec6fdaf352984b2c7
zhangpanzhao <zhangpanzhao@okay.cn>
2020-05-08 08:06:21 +0800

    [update] okhttp最简使用方式，并创建相关类，为模仿okhttp做准备

--- app/src/main/java/com/dapan/app/MainActivity.java
+++ app/src/main/java/com/dapan/app/MainActivity.java
@@ -3,11 +3,34 @@ package com.dapan.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().build();
+        Call newCall = client.newCall(request);
+        newCall.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/dapan/okhttp/Callback.java
@@ -0,0 +1,8 @@
+package com.dapan.okhttp;
+
+/**
+ * Created by per4j
+ * on 2020/5/8
+ */
+public interface Callback {
+}
